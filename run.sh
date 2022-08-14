ec041a1a009213f9257a7215bffc10abc51931ff
KotaShimomura <shimonarudo@gmail.com>
2022-08-14 23:12:39 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #bash scripts/cityscapes/hrnet/run_h_48_d_4.sh val hrnet_w48_ocr_1_latest.pth
 
-bash scripts/cityscapes/hrnet/run_h_48_d_4_ocr.sh val hrnet_w48_ocr_1_latest.pth
+bash scripts/cityscapes/hrnet/run_h_48_d_4_proto.sh val hrnet_w48_proto_lr1x_hrnet_proto_80k_latest.pth
+
